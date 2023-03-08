fb96c7f7be7ed0f59601682f804491ae75b2516e
moqing <jchz_cn@163.com>
2023-03-08 22:25:29 +0800

    feat:新增市场/浏览器/社区三个页面模板,以及pinia状态管理

---
+++ components/HomeView/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+
+/*测试store*/
+const test = useStore.test()
+const { test_text } = storeToRefs(test)
+console.log(test_text.value)
+</script>
+
+<template>
+  <div>
+    <!-- Home头部 -->
+    <HomeViewHomeHeader />
+    <!-- Home中间部分 -->
+    <HomeViewHomeMain />
+    <!-- Home底部 -->
+    <HomeViewHomeFooter />
+  </div>
+</template>
+
+<style></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
-});
+  modules: [
+    '@nuxtjs/tailwindcss',
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: [
+          // 自动引入 `usePinia()`
+          'defineStore',
+          // 自动引入 `usePinia()` 并重命名为 `usePiniaStore()`
+          ['defineStore', 'definePiniaStore'],
+        ],
+      },
+    ],
+  ],
+})
